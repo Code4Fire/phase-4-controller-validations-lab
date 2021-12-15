070675b0d5d70eff024b507e675dd9ba72653000
Tiffany Tam <tamtiffany89@gmail.com>
2021-12-14 22:52:50 -0500

    commit tree c95b7a73e1f803e756ab0ce2c4c9eccf3f931f32

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,15 +1,19 @@
-class AuthorsController < ApplicationController
+ class AuthorsController < ApplicationController
   
   def show
     author = Author.find(params[:id])
-
+    # if author
     render json: author
+    # else
+    # render json: { error: "422" }, status: :not_found
+    # end
   end
 
   def create
-    author = Author.create(author_params)
-
+    author = Author.create!(author_params)
     render json: author, status: :created
+  rescue ActiveRecord::RecordInvalid => invalid
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
   end
 
   private
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,16 +2,19 @@ class PostsController < ApplicationController
 
   def show
     post = Post.find(params[:id])
-    
+    if post
     render json: post
+    else
+    render json: { error: "422" }, status: :not_found  
+    end
   end
-
   def update
     post = Post.find(params[:id])
-
-    post.update(post_params)
-
+    post.update!(post_params)
     render json: post
+  rescue ActiveRecord::RecordInvalid => invalid
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
+    
   end
 
   private
